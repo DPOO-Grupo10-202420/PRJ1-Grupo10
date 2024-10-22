75f9125652aca431d6942e7ce3452815cc006641
MrTorresM <mr.torres@uniandes.edu.co>
2024-10-21 20:02:11 -0500

    Estudiante

---
+++ src/LearningManagementSystem/mainManagementSystem/users/Estudiante.java
@@ -0,0 +1,79 @@
+//====================================================================================
+// Definicion del package e importacion de modulos
+//====================================================================================
+
+package LearningManagementSystem.mainManagementSystem.users;
+
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+// import Usuario;
+
+
+//====================================================================================
+// Definicion de la clase Estudiante
+//====================================================================================
+
+public class Estudiante extends Usuario  {
+    
+	//----------------------------------------------------------------------
+	// Definicion de atributos
+	
+    private LearningPath studentCurrentLearningPath;
+    private HashMap<String, LearningPath> completedLearningPaths;
+
+	//----------------------------------------------------------------------
+	// Metodo constructor de la clase.
+    
+    public Profesor(String username, String password, String email) {
+
+        super(username, password,email);
+
+    }
+
+	//----------------------------------------------------------------------
+    // Definicion de metodos de la clase
+    
+
+    // Getters
+
+
+    public void crearActividad(){
+
+    }
+
+    public void crearLearingPath(){
+
+    }
+    
+    public void clonarActividad(){
+
+    }
+
+    public void cambiarEstadoActividad() {
+
+    }
+
+    public void visualizarEncuestas() {
+
+    }
+
+    public void addPreguntaEncuesta(){
+
+    }
+
+    public void addFeedBack(){
+
+    }
+        
+
+
+
+
+
+
+
+}
