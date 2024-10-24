d8ef2e929dcc93897f441a2152f9a67c4c4bd9a8
MrTorresM <mr.torres@uniandes.edu.co>
2024-10-23 21:09:49 -0500

    Def consolas

---
+++ src/LearningManagementSystem/console/consolaEstudiante.java
@@ -0,0 +1,21 @@
+package LearningManagementSystem.console;
+
+public class consolaEstudiante {
+
+    //------------------------------------------------------
+    // Definicion de los atributos
+
+    private final String[] opcionesMenuEstudiante = new String[]{ "Ingresar como ESTUDIANTE", "Ingresar como PROFESOR", "Salir" };
+
+
+    //------------------------------------------------------
+    // Definicion del metodo que ejecuta la consola del estudiante.
+
+    public void ejecutarConsolaEstudiante(){}
+
+
+
+
+
+
+}
