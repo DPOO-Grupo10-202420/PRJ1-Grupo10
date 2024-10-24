79a3aec14c5bded128f7288a0cb37fe87cdf14a0
MrTorresM <mr.torres@uniandes.edu.co>
2024-10-23 23:50:36 -0500

    Avances consola v2

--- src/LearningManagementSystem/mainManagementSystem/users/Profesor.java
+++ src/LearningManagementSystem/mainManagementSystem/users/Profesor.java
@@ -1,3 +1,4 @@
+
 //====================================================================================
 // Definicion del package e importacion de modulos
 //====================================================================================
@@ -9,7 +10,8 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
-import LearningManagementSystem.mainManagementSystem.*;
+
+import LearningManagementSystem.mainManagementSystem.LearningPath;
 import LearningManagementSystem.mainManagementSystem.activities.Actividad;
 
 // import Usuario;
@@ -19,45 +21,66 @@ import LearningManagementSystem.mainManagementSystem.activities.Actividad;
 // Definicion de la clase Profesor
 //====================================================================================
 
-public class Estudiante extends Usuario  {
+public class Profesor extends Usuario  {
     
 	//----------------------------------------------------------------------
 	// Definicion de atributos
-    private LearningPath studentCurrentLearningPath;
-    private Actividad studentCurrentActivity;
-    private HashMap<String, LearningPath> completedLearningPaths;
+	
 
 	//----------------------------------------------------------------------
 	// Metodo constructor de la clase.
-    public Estudiante(String username, String password, String email) {
+    
+    public Profesor(String username, String password, String email) {
         super(username, password,email);
     }
 
 	//----------------------------------------------------------------------
     // Definicion de metodos de la clase
+    
+
     // Getters
+
+
+    public void crearActividad(){
+
+    }
+
+    public void crearLearingPath(){
+
+    }
+    
+    public void clonarActividad(){
+
+    }
+
+    public void cambiarEstadoActividad(Actividad actividad, String estado) {
+        actividad.setEstado(estado); // impelementar setEstado
+        System.out.println("El estado de la actividad ha sido cambiado a: " + estado);
+    }
+
+
+    public void visualizarEncuestas() {
+
+    }
+
+    public void addPreguntaEncuesta(){
+
+    }
+
     public void addFeedBack(){
-        // TODO implementar donde se que vaya el feedback
+
     }
 
-    public void iniciarLearningPath(LearningPath learningPath){
-        this.studentCurrentLearningPath = learningPath;
-    }
 
-    public void salirseLearningPath(){
-        this.studentCurrentLearningPath = null;
+    
+    public LearningPath crearLearningPath(String titulo, String descripcion) {
+        return new LearningPath(titulo, descripcion);
     }
+    
+    // Clonacion -- Falta 
 
-    public void finalizarLearningPath(){
-        this.completedLearningPaths.put(this.studentCurrentLearningPath.getTitulo(), this.studentCurrentLearningPath);
-        this.studentCurrentLearningPath = null;
-    }
 
-    public void iniciarActividad(Actividad actividad){
-        this.studentCurrentActivity = actividad;
-    }
 
-    public void terminarActividad(){
-        this.studentCurrentActivity = null;
-    }
+
+
 }
